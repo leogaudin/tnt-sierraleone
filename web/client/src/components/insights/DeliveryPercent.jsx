46527f2b301d6828eaa24380b93c8b07af7a770d
Léo <leogaudinoff@gmail.com>
2024-04-01 16:24:41 +0200

    style: change delivery percentage presentation

--- web/client/src/components/insights/DeliveryPercent.jsx
+++ web/client/src/components/insights/DeliveryPercent.jsx
@@ -1,19 +1,40 @@
 import React from 'react';
-import { Typography, LinearProgress } from '@mui/material';
+import { Typography, LinearProgress, Stack, Icon, SvgIcon, Tooltip } from '@mui/material';
 import { useTranslation } from 'react-i18next';
+import InfoIcon from '@mui/icons-material/Info';
 import { getStatusPercentage } from '../../service/statistics';
 
-export default function DeliveryPercent({ sample, sampleName}) {
+export default function DeliveryPercent({ sample }) {
 	const { t } = useTranslation();
-	const deliveryPercentage = parseFloat(getStatusPercentage(sample, "delivered"));
 
+	const percentages = [
+		'validated',
+	];
 	return (
 		<>
-			<Typography variant="subtitle1">{sampleName}</Typography>
-			<Typography variant="h4" component="div" gutterBottom>
-				{deliveryPercentage.toFixed(2)}% <span style={{fontWeight: 200, fontSize: 'smaller'}}>{t('delivered').toLowerCase()}</span>
-			</Typography>
-			<LinearProgress color={'success'} variant="determinate" value={deliveryPercentage} />
+			{percentages.map((status, i) => {
+				const percentage = parseFloat(getStatusPercentage(sample, status));
+				return (
+					<>
+						<Stack
+							direction="row"
+							alignItems="center"
+							spacing={1}
+							mb={2}
+						>
+							<Typography variant="h4" component="div" gutterBottom key={i}>
+								{percentage.toFixed(2)}% <span style={{fontWeight: 200, fontSize: 'smaller'}}>{t(status).toLowerCase()}</span>
+							</Typography>
+							<Tooltip placement='top' title={t('validatedExplanation')}>
+								<SvgIcon sx={{opacity: .7}} fontSize={'.5rem'}>
+									<InfoIcon />
+								</SvgIcon>
+							</Tooltip>
+						</Stack>
+						<LinearProgress color={'success'} variant="determinate" value={percentage} />
+					</>
+				);
+			})}
 		</>
 	);
 }
