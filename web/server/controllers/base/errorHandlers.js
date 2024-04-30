1044f6857e8ce55ac29e71ade225ebaa613ec892
Léo <leogaudinoff@gmail.com>
2024-04-30 12:55:55 +0200

    fix: add safety check

--- web/server/controllers/base/errorHandlers.js
+++ web/server/controllers/base/errorHandlers.js
@@ -1,39 +1,53 @@
 const handle404Error = (res) => {
-	return res.status(404).json({ success: false, error: `Item not found` });
+	if (!res.headersSent) {
+		return res.status(404).json({ success: false, error: `Item not found` });
+	}
 };
 
 const handle401Error = (res, error) => {
-	return res.status(401).json({ success: false, error });
+	if (!res.headersSent) {
+		return res.status(401).json({ success: false, error });
+	}
 };
 
 const handle400Error = (res, error) => {
-	return res.status(400).json({ success: false, error });
+	if (!res.headersSent) {
+		return res.status(400).json({ success: false, error });
+	}
 };
 
 const handle409Error = (res, error) => {
-	return res.status(409).json({ success: false, error });
+	if (!res.headersSent) {
+		return res.status(409).json({ success: false, error });
+	}
 };
 
 const handle201Success = (res, message, invalidInstances, validInstances) => {
-	return res.status(201).json({
-		success: true,
-		message,
-		invalidInstances,
-		validInstances,
-	});
+	if (!res.headersSent) {
+		return res.status(201).json({
+			success: true,
+			message,
+			invalidInstances,
+			validInstances,
+		});
+	}
 };
 
 const handle206Success = (res, message, invalidInstances, validInstances) => {
-	return res.status(206).json({
-		success: true,
-		message,
-		invalidInstances,
-		validInstances,
-	});
+	if (!res.headersSent) {
+		return res.status(206).json({
+			success: true,
+			message,
+			invalidInstances,
+			validInstances,
+		});
+	}
 };
 
 const handle200Success = (res, data) => {
-	return res.status(200).json({ success: true, data });
+	if (!res.headersSent) {
+		return res.status(200).json({ success: true, data });
+	}
 };
 
 module.exports = {
