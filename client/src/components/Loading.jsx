288477d5d0c5e622fcd9a93e514d5790ce7e5abe
Léo <leogaudinoff@gmail.com>
2024-10-19 16:54:39 +0100

    chore and style

---
+++ client/src/components/Loading.jsx
@@ -0,0 +1,28 @@
+import { Flex, Spinner, Text } from "@chakra-ui/react";
+
+export default function Loading({
+	message = '',
+}) {
+	return (
+		<Flex
+			width='100%'
+			height='100%'
+			justify='center'
+			align='center'
+		>
+			<Spinner
+				// color={palette.primary.dark}
+			/>
+			{message
+				? <Text
+					// color={palette.primary.dark}
+					textTransform='uppercase'
+					fontSize='small'
+				>
+					{message}
+				</Text>
+				: null
+			}
+		</Flex>
+	)
+}
