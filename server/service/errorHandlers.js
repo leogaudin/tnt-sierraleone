4eb2c73060d3cde0d424106344ba248b3db21da9
Léo <leogaudinoff@gmail.com>
2024-11-07 13:31:42 +0100

    fix: add safety

--- server/service/errorHandlers.js
+++ server/service/errorHandlers.js
@@ -1,29 +1,29 @@
 export const handle404Error = (res) => {
-	if (!res.headersSent) {
+	if (res && !res.headersSent) {
 		return res.status(404).json({ success: false, error: `Item not found` });
 	}
 };
 
 export const handle401Error = (res, error) => {
-	if (!res.headersSent) {
+	if (res && !res.headersSent) {
 		return res.status(401).json({ success: false, error });
 	}
 };
 
 export const handle400Error = (res, error) => {
-	if (!res.headersSent) {
+	if (res && !res.headersSent) {
 		return res.status(400).json({ success: false, error });
 	}
 };
 
 export const handle409Error = (res, error) => {
-	if (!res.headersSent) {
+	if (res && !res.headersSent) {
 		return res.status(409).json({ success: false, error });
 	}
 };
 
 export const handle201Success = (res, message, data) => {
-	if (!res.headersSent) {
+	if (res && !res.headersSent) {
 		return res.status(201).json({
 			success: true,
 			message,
@@ -33,7 +33,7 @@ export const handle201Success = (res, message, data) => {
 };
 
 export const handle206Success = (res, message, data) => {
-	if (!res.headersSent) {
+	if (res && !res.headersSent) {
 		return res.status(206).json({
 			success: true,
 			message,
@@ -43,7 +43,7 @@ export const handle206Success = (res, message, data) => {
 };
 
 export const handle200Success = (res, data) => {
-	if (!res.headersSent) {
+	if (res && !res.headersSent) {
 		return res.status(200).json({ success: true, data });
 	}
 };
