4c1f76a05edc657a17a9fc5ab6460f5e4bf40391
Léo <leogaudinoff@gmail.com>
2024-11-20 19:15:16 +0100

    Revert "refactor"
    This reverts commit 0e5daeccf832654281a7729cfe96248949e39fcc.

--- client/src/pages/Home/components/Timeline.jsx
+++ client/src/pages/Home/components/Timeline.jsx
@@ -7,9 +7,8 @@ import {
 	ResponsiveContainer
 } from 'recharts';
 import { useTranslation } from 'react-i18next';
-import { Flex, Heading, Stack } from '@chakra-ui/react';
+import { Flex } from '@chakra-ui/react';
 import { progresses } from '../../../service';
-import { palette } from '../../../theme';
 
 const getPercent = (value, total) => {
 	const ratio = total > 0 ? value / total : 0;
@@ -23,15 +22,12 @@ export default function Timeline({
 	const { t } = useTranslation();
 
 	if (!data || !data.length) return null;
-	const repartition = data[data.length - 1];
 
 	const renderTooltipContent = o => {
 		const {
 			payload,
 			label
 		} = o;
-		if (!payload || !payload.length) return null;
-
 		const total = payload.reduce((result, entry) => result + entry.value, 0);
 		return (
 			<div
@@ -63,75 +59,32 @@ export default function Timeline({
 	};
 
 	return (
-		<>
-			<Flex
-				height={height}
-			>
-				<ResponsiveContainer width='100%' height='100%'>
-					<AreaChart
-						data={data}
-					>
-						<XAxis dataKey='name' />
-						<YAxis />
-						<Tooltip content={renderTooltipContent} />
-						{progresses.toReversed().map((progress, i) => {
-							if (progress.inTimeline) {
-								return (
-									<Area
-										key={i}
-										type='monotone'
-										dataKey={progress.key}
-										stackId='1'
-										stroke={progress.color}
-										fill={progress.color}
-									/>
-								)
-							}
-						})}
-					</AreaChart>
-				</ResponsiveContainer>
-			</Flex>
-			<Stack
-				align='center'
-				textAlign='center'
-				padding={5}
-			>
-				<Heading
-					color={palette.gray.main}
-					fontWeight='light'
+		<Flex
+			height={height}
+		>
+			<ResponsiveContainer width='100%' height='100%'>
+				<AreaChart
+					data={data}
 				>
-					{t('currently')}
-				</Heading>
-				<Stack
-					direction={{ base: 'column', md: 'row' }}
-					gap={5}
-					padding={5}
-					width='100%'
-					justify='center'
-				>
-					{Object.keys(repartition).map((key, i) => {
-						if (key === 'name') return null;
-						const progress = progresses.find(p => p.key === key);
-						return (
-							<Stack
-								color={progress.color}
-								align='center'
-								key={key}
-							>
-								<Heading>
-									{repartition[key]}
-								</Heading>
-								<Heading
-									size='sm'
-									fontWeight='light'
-								>
-									{t(progress.key)}
-								</Heading>
-							</Stack>
-						);
+					<XAxis dataKey='name' />
+					<YAxis />
+					<Tooltip content={renderTooltipContent} />
+					{progresses.toReversed().map((progress, i) => {
+						if (progress.inTimeline) {
+							return (
+								<Area
+									key={i}
+									type='monotone'
+									dataKey={progress.key}
+									stackId='1'
+									stroke={progress.color}
+									fill={progress.color}
+								/>
+							)
+						}
 					})}
-				</Stack>
-			</Stack>
-		</>
+				</AreaChart>
+			</ResponsiveContainer>
+		</Flex>
 	)
 }
